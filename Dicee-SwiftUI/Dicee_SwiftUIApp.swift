cee6b80701b868cffa05e4cb3647b836e93106eb
vamsi <vamsikrishnayadav2002@gmail.com>
2022-08-09 07:49:25 +0530

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Mekala Vamsi Krishna on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
